94118a2e1fb1d1c944e407ad0519aa5cabb3c64e
ariel.karlen@possumus.tech <ariel.karlen@possumus.tech>
2022-07-12 20:47:22 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import { Container } from "react-bootstrap";
+import "./App.scss";
+// import { Container } from "react-bootstrap";
+import NavBar from "./components/Commons/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <Container fluid>
-        <h1>Hello World</h1>
-      </Container>
-    </div>
+    <>
+      <NavBar />
+      <h1>Guitar Shop </h1>
+    </>
   );
 }
 
---
+++ src/components/Commons/NavBar.js
@@ -0,0 +1,53 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Guitar Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+
+            <NavDropdown title="Guitarras" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Bajos" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Accesorios" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
